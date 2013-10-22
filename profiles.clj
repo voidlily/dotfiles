2b61ddee8af8975f6946d517a9f49845e76af0e0
Lily <lily@voidlily.net>
2013-10-22 15:40:11 -0700

    Replace @ in clj-stacktrace with deref

--- profiles.clj
+++ profiles.clj
@@ -4,7 +4,7 @@
                                          'print-cause-trace)
                         new (ns-resolve (doto 'clj-stacktrace.repl require)
                                         'pst)]
-                    (alter-var-root orig (constantly @new)))]
+                    (alter-var-root orig (constantly (deref new))))]
    :plugins      [[codox "0.6.4"]
                   [lein-marginalia "0.7.1"]
                   [lein-try "0.3.2"]
