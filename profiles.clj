fe878148f03db67d7fbccc247cd974c67c398188
Lily <lily@voidlily.net>
2014-01-06 15:16:31 -0800

    add lein-licenses

--- profiles.clj
+++ profiles.clj
@@ -9,4 +9,5 @@
                   [lein-marginalia "0.7.1"]
                   [lein-try "0.3.2"]
                   [lein-kibit "0.0.8"]
-                  [lein-ancient "0.5.1"]]}}
+                  [lein-ancient "0.5.1"]
+                  [lein-licenses "0.1.1"]]}}
