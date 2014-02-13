57394b43043701d5f774808f3809abbef46a6d60
Lily <lily@voidlily.net>
2014-02-12 16:30:36 -0800

    upgrade lein plugins

--- profiles.clj
+++ profiles.clj
@@ -5,11 +5,14 @@
                         new (ns-resolve (doto 'clj-stacktrace.repl require)
                                         'pst)]
                     (alter-var-root orig (constantly (deref new))))]
-   :plugins      [[codox "0.6.4"]
+   :plugins      [
+                  [codox "0.6.6"]
                   [lein-marginalia "0.7.1"]
-                  [lein-try "0.3.2"]
+                  [lein-try "0.4.1"]
                   [lein-kibit "0.0.8"]
-                  [lein-ancient "0.5.1"]
+                  [lein-ancient "0.5.4"]
                   [lein-licenses "0.1.1"]
-                  [jonase/eastwood "0.0.2"]
-                  ]}}
+                  [jonase/eastwood "0.1.0"]
+                  [lein-pprint "1.1.1"]
+                  ]
+   }}
