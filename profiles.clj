99609c17f1156b83aa9386593a512a3368cc22ea
Lily <voidlily@users.noreply.github.com>
2018-03-06 13:29:24 -0800

    Update lein plugins

--- profiles.clj
+++ profiles.clj
@@ -1,20 +1,20 @@
 {:user
-  {:dependencies [[clj-stacktrace "0.2.7"]]
+  {:dependencies [[clj-stacktrace "0.2.8"]]
    :injections   [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                          'print-cause-trace)
                         new (ns-resolve (doto 'clj-stacktrace.repl require)
                                         'pst)]
                     (alter-var-root orig (constantly (deref new))))]
    :plugins      [
-                  [codox "0.6.6"]
-                  [lein-marginalia "0.7.1"]
-                  [lein-try "0.4.1"]
-                  [lein-kibit "0.0.8"]
-                  [lein-ancient "0.5.4"]
-                  [lein-licenses "0.1.1"]
-                  [jonase/eastwood "0.1.0"]
-                  [lein-pprint "1.1.1"]
-                  [lein-exec "0.3.4"]
-                  [lein-simpleton "1.2.0"]
+                  [codox "0.10.3"]
+                  [lein-marginalia "0.9.1"]
+                  [lein-try "0.4.3"]
+                  [lein-kibit "0.1.6"]
+                  [lein-ancient "0.6.15"]
+                  [lein-licenses "0.2.2"]
+                  [jonase/eastwood "0.2.5"]
+                  [lein-pprint "1.2.0"]
+                  [lein-exec "0.3.7"]
+                  [lein-simpleton "1.3.0"]
                   ]
    }}
