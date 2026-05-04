aede93508e66bf46b4b96e2476626a06c16f9eee
Lily <voidlily@users.noreply.github.com>
2026-05-04 11:14:29 -0700

    disable direnv checkphase on darwin, broken
    https://github.com/NixOS/nixpkgs/issues/507531
    https://github.com/fredsystems/nixos/commit/3ed64d93c707810088fdab968ac00828c0f4af6a

---
+++ overlays/direnv/default.nix
@@ -0,0 +1,18 @@
+{
+  ...
+}:
+
+final: prev: {
+  # direnv builds fail on darwin due to zsh tests hanging indefinitely
+  # tests pass on linux so just disable checkPhase on darwin to get things
+  # moving again
+  # https://github.com/NixOS/nixpkgs/issues/507531
+  # https://github.com/fredsystems/nixos/commit/3ed64d93c707810088fdab968ac00828c0f4af6a
+  direnv =
+    if prev.stdenv.isDarwin then
+      prev.direnv.overrideAttrs (_: {
+        doCheck = false;
+      })
+    else
+      prev.direnv;
+}
