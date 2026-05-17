040e6cd414fc0cedb7746c6592f6411bd0221c91
Lily <voidlily@users.noreply.github.com>
2026-05-16 18:24:13 -0700

    update nix-darwin input github org

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
