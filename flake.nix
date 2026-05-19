21a0c786233c171621323a20645778db34fff7ec
Lily <voidlily@users.noreply.github.com>
2026-05-18 20:01:23 -0700

    repeat for darwin

--- flake.nix
+++ flake.nix
@@ -99,6 +99,8 @@
           ./legacymodules
           ./users
           ./hosts/homu/_homeManager
+          ./hosts/lilys-MacBook-Pro/_darwin
+          ./hosts/Lily-Rappaport-TM1069/_darwin
         ]
       );
   # imports = [
