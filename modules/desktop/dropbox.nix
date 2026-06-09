72c35c184c0911772f4d4dacbea50745235fce1a
Lily <voidlily@users.noreply.github.com>
2026-06-09 16:08:57 -0700

    fix dropbox

--- modules/desktop/dropbox.nix
+++ modules/desktop/dropbox.nix
@@ -16,21 +16,8 @@
         "firefox-bin-unwrapped"
       ])
     ];
-    homeManager =
-      { pkgs, ... }:
-      {
-        systemd.user.services.dropbox = {
-          Unit = {
-            Description = "Dropbox service";
-          };
-          Install = {
-            WantedBy = [ "default.target" ];
-          };
-          Service = {
-            ExecStart = "${pkgs.dropbox}/bin/dropbox";
-            Restart = "on-failure";
-          };
-        };
-      };
+    homeManager = {
+      services.dropbox.enable = true;
+    };
   };
 }
