f2abbab1e62782554ef621c0c160324e66abe9ff
Lily <voidlily@users.noreply.github.com>
2026-06-15 09:55:31 -0700

    emacs: just-mode

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -13,6 +13,7 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
 (package! rainbow-delimiters)
+(package! just-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
