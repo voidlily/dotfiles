babeb032012523efcecf2448c050edf5ddc3b0d7
Lily <voidlily@users.noreply.github.com>
2026-07-31 12:27:24 -0700

    reenable batgrep

--- modules/shell/bat.nix
+++ modules/shell/bat.nix
@@ -15,8 +15,7 @@
           extraPackages = with pkgs.bat-extras; [
             batdiff
             batman
-            # TODO broken - https://github.com/NixOS/nixpkgs/issues/454391
-            # batgrep
+            batgrep
             batwatch
             prettybat
           ];
