5ac9f140579dd1ac3225afa92f4220ff2c85d9e8
Lily <voidlily@users.noreply.github.com>
2026-09-02 16:28:36 -0700

    podman: increase timeout in systemd unit
    https://buildpacks.io/docs/for-app-developers/how-to/special-cases/build-on-podman/

--- modules/containers.nix
+++ modules/containers.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   ...
 }:
 
@@ -21,6 +22,23 @@
           };
         };
         environment.systemPackages = [ pkgs.podman-compose ];
+
+        # https://buildpacks.io/docs/for-app-developers/how-to/special-cases/build-on-podman/
+        systemd.services.podman.serviceConfig = {
+          # ExecStart in a list sets multiple lines
+          # ExecStart=
+          # ExecStart=/nix/store/...
+          #
+          # ^ is needed to override a service's execstart command cause
+          # otherwise it complains about multiple conflicting values
+          #
+          # explainer on this syntax:
+          # https://github.com/NixOS/nixpkgs/issues/63703#issuecomment-504836857
+          ExecStart = [
+            ""
+            "${lib.getExe pkgs.podman} $LOGGING system service --time=1800"
+          ];
+        };
       };
     provides.to-users = {
       # if i care enough, limit this by username, otherwise it's available to
